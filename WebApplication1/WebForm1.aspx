90c4a12473ce7fc22a451915766a78e9c29a4991
TanhuKowski <khanishlielshad@gmail.com>
2022-11-27 14:59:32 +0400

    Add project files.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+            <div>
+        <asp:Label ID="lbl_cname" runat="server" Text="CategoryName"></asp:Label>
+        <asp:TextBox ID="txt_cname" runat="server" style="margin-left: 11px"></asp:TextBox>
+        <asp:Button ID="btn_search" runat="server" Text="Search" OnClick="btn_search_Click" style="margin-left: 21px" />
+        <asp:GridView ID="grid" runat="server" style="margin-top: 37px" AutoGenerateColumns="False" Height="16px" OnSelectedIndexChanged="grid_SelectedIndexChanged" Width="424px">
+                </asp:GridView>
+            </div>
+            <p>
+                <asp:Label ID="lbl_result" runat="server" Text=""></asp:Label>
+            </p>
+    </form>
+
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection("Server=MSI\\SQLEXPRESS;Integrated Security=true;Database=Northwind;");
+        SqlCommand cmd;
+        SqlCommand cmd1;
+        string query;
+        private DataTable dt = new DataTable();
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_search_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("Search_procedure", conn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", txt_cname.Text);
+            cmd.Parameters.Add(new SqlParameter()
+            {
+                Direction = ParameterDirection.Output,
+                ParameterName = "@status",
+                SqlDbType = SqlDbType.Int
+            });
+            cmd.ExecuteNonQuery();
+            if (cmd.Parameters["@status"].Value.ToString() == "1")
+            {
+                query = "select * from View_inner where CategoryName='" + txt_cname.Text + "'";
+                cmd1 = new SqlCommand(query, conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd1);
+                adapter.Fill(dt);
+                grid.DataSource = dt;
+                grid.DataBind();
+            }
+            else
+            {
+                lbl_result.Text = "Data does not exist!";
+            }
+            conn.Close();
+        }
+
+        protected void grid_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
